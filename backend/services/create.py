a402043f1f08e2530131f3abe7a21fc81b201984
William Shi <33271663+wx02shi@users.noreply.github.com>
2024-11-23 19:44:51 -0500

    wip

--- backend/services/create.py
+++ backend/services/create.py
@@ -1,7 +1,28 @@
+from typing import List, Optional
 from db import db_client
 
 
 def create(
-    address: str, description: str, size: int, listing_price: float, metadata=None
+    address: str,
+    sqft: int,
+    listing_price: float,
+    coordinates: List[float],
+    metadata: Optional[dict] = None,
 ):
-    db_client.table("Property").insert({""})
+    # Generate a description of the listing from photos
+    description = ""
+
+    # Generate an embedding of the description
+    embedding = ""
+
+    db_client.table("Property").insert(
+        {
+            "address": address,
+            "description": description,
+            "sqft": sqft,
+            "listing_price": listing_price,
+            "embedding": embedding,
+            "coordinates": coordinates,
+            "metadata": metadata,
+        }
+    )
