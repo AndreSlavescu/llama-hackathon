7acfa2c6106b022f776e5759db9aaa92203947d6
William Shi <33271663+wx02shi@users.noreply.github.com>
2024-11-23 19:17:39 -0500

    stuff

--- backend/db.py
+++ backend/db.py
@@ -3,4 +3,4 @@ from supabase import create_client, Client
 
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
-supabase: Client = create_client(url, key)
+db_client: Client = create_client(url, key)
