5c5bd27872bc0805b838aba6ec7e0253f87c5487
William Shi <wx02shi@icloud.com>
2024-11-24 00:25:23 +0000

    basic gen endpoint

--- backend/main.py
+++ backend/main.py
@@ -2,11 +2,19 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 import numpy as np
 
+# llama model
+from llm_generation.generate import TextGenerator
+
 from db import get_db
 from schemas.requests import SearchRequest
 
 app = FastAPI()
+generator = TextGenerator()
 
+@app.post("/generate/")
+def generate_text(prompt: str):
+    text = generator.generate(prompt)
+    return {"text": text}
 
 @app.post("/search/")
 def search_properties(search: SearchRequest, db: Session = Depends(get_db)):
